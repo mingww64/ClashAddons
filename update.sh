12779422c0aaa8e6b8e8d04d0d9b263df89a0dbe
FeliciaWen <felicia@realnet.ml>
2021-08-22 22:12:12 +0800

    test provider update action

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir proxy_provider
+
+link=($(grep -o 'http.*sub.*' ./config.yaml))
+name=($(grep -o './proxy_provider.*.yaml' ./config.yaml |awk -F/ '{print $3}'))
+nnum=${#name[@]}
+lnum=${#link[@]}
+if [ $nnum != $lnum ];then
+    echo "Url ≠ Name"
+    exit 1
+fi
+
+for num in $(seq 0 $nnum);do
+    if [ $num == $nnum ];then
+        echo "Completed. "
+        exit 0
+    fi
+    curl -SsL ${link[$num]} -o tmp
+    if [ ! -z $(grep proxies tmp) ];then
+        echo "Writing ${name[$num]}"
+        mv tmp proxy_provider/${name[$num]}
+    else
+        rm tmp
+    fi
+done
