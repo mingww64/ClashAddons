b33b1f7d5cbe4e41dc1b992850776c8f0c9efb68
FeliciaWen <felicia@realnet.ml>
2021-11-20 20:16:34 +0800

    v2

--- update.sh
+++ update.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
-link=($(grep -o 'http.*sub.*' ./Orginal.yaml))
-name=($(grep -o './proxy_provider.*.yaml' ./Orginal.yaml |awk -F/ '{print $3}'))
-nnum=${#name[@]} # Numbers of providers
-lnum=${#link[@]} # Numbers of subscribe links
-if [ $nnum != $lnum ];then
-    echo "Url ≠ Name"
-    exit 1
-fi
-
+config=./config.yaml
+getconf(){
+    yq e $1 $2 $config 
+}
+QuickGen(){
+    echo QuickGenQX Activated.
+    location=$(readlink -f proxies/QuantumultX/AIO)
+    dir=$(dirname $location)
+    rm $location
+    names=$(ls $dir)
+    for x in $names;do
+        if [ $x == AIO ];then continue; fi
+        echo -e "\n#$x" >> $location
+        cat $dir/$x >> $location
+    done
+    curl -SsL "$server/sub?url=$location&target=quanx&config=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FSleepyHeeead%2Fsubconverter-config%40master%2Fremote-config%2Funiversal%2Furltest.ini&emoji=true" -o ./QuantumultX.conf
+    sed -i "s/"$server"/"$remote_server"/g" ./QuantumultX.conf 
+}
+server=$(getconf .SCServer)
+remote_server=$(getconf .SCServerRemote)
+p_name=($(echo $(getconf ".ProxyProviders|keys"|sed "s/- //")))
+p_url=($(echo $(getconf .ProxyProviders.[])))
+clash_args=$(echo $(getconf .ClashProviders|sed "s/: /=/")|sed "s/ /\&/g")
+quanx_args=$(echo $(getconf .QuantumultXRemotes|sed "s/: /=/")|sed "s/ /\&/g")
+filters=$(getconf .ExcludeExp)
+nnum=${#p_name[@]} # Numbers of providers
+lnum=${#p_url[@]} # Numbers of subscribe links
+checknode="The following link doesn't contain any valid node info:|No nodes were found!"
 for num in $(seq 0 $nnum);do
     if [ $num == $nnum ];then
+        if [ $(getconf .QuickGenQX) == true ];then QuickGen; fi
         echo "Completed. "
         git add -A
         if [ -z "$(git status -u |grep "Changes to be committed:")" ];then
@@ -21,11 +41,19 @@ for num in $(seq 0 $nnum);do
         fi
         exit 0
     fi
-    curl -SsL ${link[$num]} -o tmp
-    if [ ! -z "$(grep proxies tmp)" ];then # Check if any proxies available
-        echo "Writing ${name[$num]}"
-        mv tmp proxy_provider/${name[$num]}
+    curl -SsL "$server/sub?url=${p_url[$num]}&$clash_args&$filters" -o tmpc
+    curl -SsL "$server/sub?url=${p_url[$num]}&$quanx_args&$filters" -o tmpq
+
+    if [ -z "$(grep -E "$checknode" tmpc)" ];then # Check if any proxies available
+        echo "Clash: Writing ${p_name[$num]}"
+        mv -f tmpc proxies/Clash/${p_name[$num]}
     else
-        rm tmp # Keep last file and make no change
+        rm tmpc # Keep last file and make no change
+    fi
+    if [ -z "$(grep -E "$checknode" tmpq)" ];then
+        echo "QuantumultX: Writing ${p_name[$num]}"
+        mv -f tmpq proxies/QuantumultX/${p_name[$num]}
+    else
+        rm tmpq
     fi
 done
