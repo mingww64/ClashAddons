39c4ad0935a4eecaf4fa0d0619dc08e221d07c35
FeliciaWen <wmyfelix@gmail.com>
2022-02-08 15:42:54 +0800

    fix duplicate region

--- omc/region.py
+++ omc/region.py
@@ -20,14 +20,16 @@ def processor(path, out = "", exclude = '限速|游戏|game'):
     os.makedirs(out,exist_ok=True)
     parsed = requests.get('https://raw.githubusercontent.com/tindy2013/subconverter/master/base/snippets/emoji.txt', allow_redirects=True).content.decode('utf-8')
     re_list = parsed.strip().split('\n')
+    proxies_list = gather_files(path, exclude)
     for num, re_ in list(enumerate(re_list)): # make a list of each region proxies and put them in for better flexbility.
         re_match = re_.split(',')[0]
         re_emoji = re_.split(',')[1]
         locals()[f'list_{num}'] = []
         list_reg = locals()[f"list_{num}"] # use local() func mannally, otherwise would be recognize as str. because var is spliced by string.format.
-        for proxies in gather_files(path, exclude):
+        for proxies in proxies_list:
             if re.search(re_match,proxies):
                 list_reg.append(proxies)
+                proxies_list.remove(proxies) # solve duplicate match (China regexp.)
         if len(list_reg) != 0:
             with open(out + re_emoji,'w') as f:
                 txt = "proxies:\n"
